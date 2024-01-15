c96fff17fdfc61d22bc0f5eb6a0aa431ffc6e713
shirls326 <63171384+shirls326@users.noreply.github.com>
2024-01-15 18:45:21 -0500

    animated work container working

--- my-personal-website/src/components/Work.js
+++ my-personal-website/src/components/Work.js
@@ -1,43 +1,88 @@
-import React from 'react'
-import './Work.css'
-import 'animate.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./Work.css";
+import "animate.css";
 
 export default function Work() {
+    const containerRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
+  
+    useEffect(() => {
+      const options = {
+        root: null,
+        rootMargin: '-50% 0px -50% 0px',
+        threshold: 0,
+      };
+  
+      const handleIntersection = (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+          }
+        });
+      };
+  
+      const observer = new IntersectionObserver(handleIntersection, options);
+  
+      if (containerRef.current) {
+        observer.observe(containerRef.current);
+      }
+  
+      return () => {
+        if (containerRef.current) {
+          observer.unobserve(containerRef.current);
+        }
+      };
+    }, []);
+  
   return (
-
-    <div className="Work" id ="Container">
-   
-
-        <div className='TA Work-Container animate__animated animate__fadeInDown'>
-            <h1 className='TA-title'>Teaching Assistant<br></br> @Lehigh University</h1>
-            <h2>Aug 2023 - Present</h2>
-            <ul className='info-list'>
-                <li>TA for "Intro to Programming" and "Data Structures and Programming"</li>
-                <li>Conduct in-class labs for 4 hours weekly</li>
-                <li>Proficiency in basic Java and advanced data structures/algorithms</li>
-                <li>Spend 5 hours weekly grading student assignments</li>
-            </ul>
-      
-        </div>
-        <div className='TA Work-Container animate__animated animate__fadeInDown'>
-            <h1 className='TA-title Zaniac-title'>Remote Developer Intern<br></br> @INDATA (Remote)</h1>
-            <h2>April 2021 - June 2021</h2>
-            <ul className='info-list'>
-                <li>Organized timestamp data using Python</li>
-                <li>Exported results to JSON format</li>
-                <li>Collaborated with supervisor from Google to advance project</li>
-            </ul>
-        </div>
-        <div className='TA Work-Container animate__animated animate__fadeInDown'>
-            <h1 className='TA-title Zaniac-title'>Math Tutor<br></br>@Zaniac (Westport, CT)</h1>
-            <h2>April 2021 - June 2021</h2>
-            <ul className='info-list'>
-                <li>Organized timestamp data using Python</li>
-                <li>Exported results to JSON format</li>
-                <li>Collaborated with supervisor from Google to advance project</li>
-            </ul>
-        </div>
-        
-  </div> 
-  )
+    <div className="Work" id="Container">
+     <div
+        ref={containerRef}
+        className={`TA Work-Container ${isVisible ? 'visible' : ''}`}
+      >
+        <h1 className="TA-title">
+          Teaching Assistant<br></br> @Lehigh University
+        </h1>
+        <h2>Aug 2023 - Present</h2>
+        <ul className="info-list">
+          <li>
+            TA for "Intro to Programming" and "Data Structures and Programming"
+          </li>
+          <li>Conduct in-class labs for 4 hours weekly</li>
+          <li>
+            Proficiency in basic Java and advanced data structures/algorithms
+          </li>
+          <li>Spend 5 hours weekly grading student assignments</li>
+        </ul>
+      </div>
+      <div
+        ref={containerRef}
+        className={`TA Work-Container ${isVisible ? 'visible' : ''}`}
+      >
+        <h1 className="TA-title Zaniac-title">
+          Remote Developer Intern<br></br> @INDATA (Remote)
+        </h1>
+        <h2>April 2021 - June 2021</h2>
+        <ul className="info-list">
+          <li>Organized timestamp data using Python</li>
+          <li>Exported results to JSON format</li>
+          <li>Collaborated with supervisor from Google to advance project</li>
+        </ul>
+      </div>
+      <div
+        ref={containerRef}
+        className={`TA Work-Container ${isVisible ? 'visible' : ''}`}
+      >
+        <h1 className="TA-title Zaniac-title">
+          Math Tutor<br></br>@Zaniac (Westport, CT)
+        </h1>
+        <h2>April 2021 - June 2021</h2>
+        <ul className="info-list">
+          <li>Organized timestamp data using Python</li>
+          <li>Exported results to JSON format</li>
+          <li>Collaborated with supervisor from Google to advance project</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
