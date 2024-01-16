4cddc97d94e870b14100262b071cc45bbfe798da
shirls326 <63171384+shirls326@users.noreply.github.com>
2024-01-15 22:02:12 -0500

    Animating home.js

--- my-personal-website/src/components/Home.js
+++ my-personal-website/src/components/Home.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import group1img from'../Group 1.png';
-import './Home.css'
+import React from 'react';
+import group1img from '../Group 1.png';
+import './Home.css';
 
 export default function Home() {
   return (
-    <div className="Home Container" id ="Home">
-        <div className = "home-Main">
-          <h1>Hello! I'm<br></br><span class="shirley animate__animated animate__fadeIn">Shirley</span>✨</h1>
-          <img src={group1img} alt="Logo" className = "star" />
-        </div>
- 
+    <div className="Home Container" id="Home">
+      <div className="home-Main">
+        <h1>
+          <span className="hello animate__animated animate__fadeIn">Hello! </span>
+          <span className="im animate__animated animate__fadeIn">I'm<br></br></span>
+          <span className="shirley shirleyfade animate__animated animate__fadeIn">Shirley</span>
+          <span className="sparkle animate__animated animate__fadeIn">✨</span>
+        </h1>
+        <img src={group1img} alt="Logo" className="star" />
       </div>
-  )
+    </div>
+  );
 }
