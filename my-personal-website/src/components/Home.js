7c803b747c36a58ff8f5be8600daec8bc7a64eb7
Shirley Angos <shirleyangos@Shirleys-MacBook-Pro.local>
2025-01-11 20:01:30 -0500

    trying to fix positioning for home

--- my-personal-website/src/components/Home.js
+++ my-personal-website/src/components/Home.js
@@ -8,27 +8,36 @@ import shirleySit from '../images/test2.svg';
 
 
 
-window.addEventListener('scroll', function() {
-  const scrollPosition = window.scrollY;
-  const parallaxText = document.querySelector('.homeText');
+// window.addEventListener('scroll', function() {
+//   const scrollPosition = window.scrollY;
+//   const parallaxText = document.querySelector('.homeText');
 
-  // Move the text at a slower rate than the scroll position
-  parallaxText.style.transform = `translate(0%, ${scrollPosition / 3}px)`;
-});
+//   // Move the text at a slower rate than the scroll position
+//   parallaxText.style.transform = `translate(0%, ${scrollPosition / 3}px)`;
+// });
 
 
 export default function Home() {
   return (
     <div className="Home Container" id="Home">
       <div className="home-Main">
-        <h1 className='homeText'>
-          <span className="hello animate__animated animate__fadeIn">Hello! </span>
-          <span className="im animate__animated animate__fadeIn">I'm<br></br></span>
-          <span className="shirley shirleyfade animate__animated animate__fadeIn">Shirley</span>
-          {/* <span className="sparkle animate__animated animate__fadeIn">✨</span> */}
-        </h1>
-        <h1 className='sparkles purple'>✦</h1>
-        <h1 className='sparkles yellow'>✦</h1>
+        <div className='homeText'>
+          <h1>
+            <span className="hello animate__animated animate__fadeIn">Hello! </span>
+            <span className="im animate__animated animate__fadeIn">I'm<br></br></span>
+            <span className="shirley shirleyfade animate__animated animate__fadeIn">Shirley</span>
+
+          </h1>
+          <h2>Hello world!</h2>
+        
+          <h3>I create beautiful design along with clean solutions. </h3>
+
+         
+        </div>
+
+
+        {/* <h1 className='sparkles purple'>✦</h1>
+        <h1 className='sparkles yellow'>✦</h1> */}
 
     
 
